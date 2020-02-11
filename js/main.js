5ab2fb0f9e3727116f01c32deeb86a1fc6233204
glocker <georgeglocker@gmail.com>
2020-02-12 01:00:46 +0700

    Устранил замечания

--- js/main.js
+++ js/main.js
@@ -1,60 +1,60 @@
-// FUNCTION TO GET RANDOM VALUE
-var random = function (min, max) {
-  min = Math.ceil(min);
-  max = Math.ceil(max);
-
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-var getRandomElementInArray = function (array) {
-  return array[random(0, array.length - 1)];
-}
-
 // CONSTS FOR mocksData
 
-var objectsNumber = 8;
-var min = 1;
-var max = 10;
-var types = ['palace', 'flat', 'house', 'bungalo'];
-var checks = ['12:00', '13:00', '14:00'];
-var mapMinWidth = 1;
-var mapMaxWidth = 3500;
-var yMin = 130;
-var yMax = 630;
-var pinSize = 40;
+var OBJECTS_NUMBER = 8;
+var MIN = 1;
+var MAX = 10;
+var TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var CHECKS = ['12:00', '13:00', '14:00'];
+var MAP_MIN_WIDTH = 1;
+var MAP_MAX_WIDTH = 3500;
+var Y_MIN = 130;
+var Y_MAX = 630;
+var PIN_SIZE = 40;
 
 var createMocks = function () {
   var mocksData = [];
 
-  for (var i = 0; i < objectsNumber.length; i++) {
+  for (var i = 0; i <= OBJECTS_NUMBER; i++) {
     mocksData.push(
       {
         'author': {
-          'avatar': '../img/avatars/user' + i + 1 + '.png'
+          'avatar': '../img/avatars/user' + 0 + i + '.png'
         },
 
         'offer': {
           'title': 'title',
           'address': 'location.x, location.y',
-          'price': random(),
-          'type': getRandomElementInArray(types),
-          'rooms': random(min, max),
-          'guests': random(min, max),
-          'checkin': getRandomElementInArray(checks),
-          'checkout': getRandomElementInArray(checks),
+          'price': random(MIN, MAX),
+          'type': getRandomElementInArray(TYPES),
+          'rooms': random(MIN, MAX),
+          'guests': random(MIN, MAX),
+          'checkin': getRandomElementInArray(CHECKS),
+          'checkout': getRandomElementInArray(CHECKS),
           'features': ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
           'description': 'desc',
           'photos': ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg']
         },
 
         'location': {
-          'x': random(mapMinWidth, mapMaxWidth),
-          'y': random(yMin, yMax)
+          'x': random(MAP_MIN_WIDTH, MAP_MAX_WIDTH),
+          'y': random(Y_MIN, Y_MAX)
         }
       }
     );
   }
   return  mocksData;
+}
+
+var mocks = createMocks();
+
+// FUNCTIONS TO GET RANDOM VALUES
+var random = function (min, max) {
+  var numbers = min + Math.random() * (max + 1 - min);
+
+  return Math.floor(numbers);
+}
+var getRandomElementInArray = function (array) {
+  return array[random(0, array.length - 1)];
 }
 
 
@@ -71,7 +71,7 @@ var mapPins = document.querySelector('.map__pins');
 var getNewAd = function () {
   var pin = pinTemplate.cloneNode(true);
 
-  createMocks.forEach(function (i, item) {
+  mocks.forEach(function (i, item) {
     pin.querySelector('.pin__img').src = item.avatar;
     pin.querySelector('.pin__img').alt = item.title;
     pin.querySelector('.map-pin').style.left = item.location.x + (-pinSize * 0.5);
