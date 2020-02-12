215b775a5552799df8f490f44b4b24332449c151
glocker <georgeglocker@gmail.com>
2020-02-12 21:07:18 +0700

    Устранил замечания 3

--- js/main.js
+++ js/main.js
@@ -16,34 +16,34 @@ var createMocks = function () {
 
   for (var i = 0; i <= OBJECTS_NUMBER; i++) {
     mocksData.push(
-      {
-        'author': {
-          'avatar': '../img/avatars/user' + 0 + i + '.png'
-        },
+        {
+          'author': {
+            'avatar': '../img/avatars/user' + 0 + i + '.png'
+          },
 
-        'offer': {
-          'title': 'title',
-          'address': 'location.x, location.y',
-          'price': random(MIN, MAX),
-          'type': getRandomElementInArray(TYPES),
-          'rooms': random(MIN, MAX),
-          'guests': random(MIN, MAX),
-          'checkin': getRandomElementInArray(CHECKS),
-          'checkout': getRandomElementInArray(CHECKS),
-          'features': ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
-          'description': 'desc',
-          'photos': ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg']
-        },
+          'offer': {
+            'title': 'title',
+            'address': 'location.x, location.y',
+            'price': random(MIN, MAX),
+            'type': getRandomElementInArray(TYPES),
+            'rooms': random(MIN, MAX),
+            'guests': random(MIN, MAX),
+            'checkin': getRandomElementInArray(CHECKS),
+            'checkout': getRandomElementInArray(CHECKS),
+            'features': ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+            'description': 'desc',
+            'photos': ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg']
+          },
 
-        'location': {
-          'x': random(MAP_MIN_WIDTH, MAP_MAX_WIDTH),
-          'y': random(Y_MIN, Y_MAX)
+          'location': {
+            'x': random(MAP_MIN_WIDTH, MAP_MAX_WIDTH),
+            'y': random(Y_MIN, Y_MAX)
+          }
         }
-      }
     );
   }
-  return  mocksData;
-}
+  return mocksData;
+};
 
 
 // FUNCTIONS TO GET RANDOM VALUES
@@ -51,10 +51,10 @@ var random = function (min, max) {
   var numbers = min + Math.random() * (max + 1 - min);
 
   return Math.floor(numbers);
-}
+};
 var getRandomElementInArray = function (array) {
   return array[random(0, array.length - 1)];
-}
+};
 
 var mocks = createMocks();
 
@@ -65,10 +65,11 @@ var map = document.querySelector('.map');
 map.classList.remove('map--faded');
 
 var pinTemplate = document.querySelector('#pin').content;
+var pinButton = pinTemplate.querySelector('button');
 var mapPins = document.querySelector('.map__pins');
 
 var getNewAd = function () {
-  var pin = pinTemplate.cloneNode(true);
+  var pin = pinButton.cloneNode(true);
 
   mocks.forEach(function (item, i) {
     pin.querySelector('img').src = item.avatar;
@@ -77,15 +78,11 @@ var getNewAd = function () {
     pin.querySelector('button').style.top = item.location.y + (-PIN_SIZE * 0.5) + 'px';
   });
   return pin;
-}
+};
 
 var fragment = document.createDocumentFragment();
 fragment.appendChild(getNewAd());
 
 
 mapPins.appendChild(fragment);
-
-
-
-
 
